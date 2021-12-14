7e55e39c86adb1025924e8cad64d81203ae69cc9
WhiteAlbum <im.xiayang.cq@gmail.com>
2021-12-14 17:04:16 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -255,7 +255,7 @@ export default {
           ClashR: "clashr",
           Surge2: "surge&ver=2",
         },
-        backendOptions: [{ value: "https://subweb-heroku.yunbai.workers.dev/sub?" }],
+        backendOptions: [{ value: "https://calm-disk-1cb4.im-xiayang-cq4550.workers.dev/sub?" }],
         remoteConfig: [
         {
             label: "ACL4SSR",
